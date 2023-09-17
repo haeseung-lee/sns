4683823d2d7608432a59e400f8b8d6bfe393d2b7
haeseung-lee <dlgotmd93@gmail.com>
2023-09-17 20:55:37 +0900

    feat : password 암호화 기능 추가

--- src/main/java/com/haeseung/sns/service/UserService.java
+++ src/main/java/com/haeseung/sns/service/UserService.java
@@ -6,6 +6,7 @@ import com.haeseung.sns.model.User;
 import com.haeseung.sns.model.entity.UserEntity;
 import com.haeseung.sns.repository.UserEntityRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -15,6 +16,8 @@ import java.util.Optional;
 public class UserService {
 
     private final UserEntityRepository userEntityRepository;
+    private final BCryptPasswordEncoder encoder;
+
     //TODO : implementes
     public User join(String userName, String password){
         //회원가입하려는 userName으로 회원가입된 user가 있는지
@@ -23,7 +26,7 @@ public class UserService {
         });
 
         //회원가입 진행 = user를 등록
-        UserEntity userEntity = userEntityRepository.save(UserEntity.of(userName, password));
+        UserEntity userEntity = userEntityRepository.save(UserEntity.of(userName, encoder.encode(password)));
 
         return User.fromEntity(userEntity);
     }
