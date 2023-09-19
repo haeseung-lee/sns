a6577bed5183aeeab34d03537e6b03d59dcbdffa
haeseung-lee <dlgotmd93@gmail.com>
2023-09-19 22:17:13 +0900

    feat : 포스트 작성 기능

---
+++ src/main/java/com/haeseung/sns/repository/PostEntityRepository.java
@@ -0,0 +1,9 @@
+package com.haeseung.sns.repository;
+
+import com.haeseung.sns.model.entity.PostEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostEntityRepository extends JpaRepository<PostEntity, Integer> {
+}
