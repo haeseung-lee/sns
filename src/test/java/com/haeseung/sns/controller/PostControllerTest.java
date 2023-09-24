3de99898310d1bd10a10c6021fdb292fb5235ad5
haeseung-lee <dlgotmd93@gmail.com>
2023-09-24 22:36:52 +0900

    test : 포스트 작성 테스트

--- src/test/java/com/haeseung/sns/controller/PostControllerTest.java
+++ src/test/java/com/haeseung/sns/controller/PostControllerTest.java
@@ -3,10 +3,12 @@ package com.haeseung.sns.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.haeseung.sns.controller.request.PostCreateRequest;
 import com.haeseung.sns.controller.request.UserJoinRequest;
+import com.haeseung.sns.service.PostService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -27,6 +29,8 @@ public class PostControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @MockBean
+    private PostService postService;
     @Test
     @WithMockUser
     void 포스트작성() throws Exception{
