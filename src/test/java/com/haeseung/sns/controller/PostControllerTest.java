18f11f5f35850e7bb961164f78d704c7e4bb38d9
haeseung-lee <dlgotmd93@gmail.com>
2023-09-28 00:09:32 +0900

    test : post 테스트 작성

--- src/test/java/com/haeseung/sns/controller/PostControllerTest.java
+++ src/test/java/com/haeseung/sns/controller/PostControllerTest.java
@@ -5,6 +5,8 @@ import com.haeseung.sns.controller.request.PostCreateRequest;
 import com.haeseung.sns.controller.request.PostModifyRequest;
 import com.haeseung.sns.exception.ErrorCode;
 import com.haeseung.sns.exception.SnsApplicationException;
+import com.haeseung.sns.fixture.PostEntityFixture;
+import com.haeseung.sns.model.Post;
 import com.haeseung.sns.service.PostService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +21,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -69,6 +72,8 @@ public class PostControllerTest {
         String title = "title";
         String body = "body";
 
+        when(postService.modify(eq(title), eq(body), any(), any())).
+                thenReturn(Post.fromEntity(PostEntityFixture.get("userName",1,1)));
         mockMvc.perform(put("/api/v1/posts/1")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsBytes(new PostModifyRequest(title, body)))
